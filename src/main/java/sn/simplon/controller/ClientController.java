2a0eb9d2368afffacf7cfaacd3cb5a00479e7ff2
Judith Lisette SAGNA <sagnajudith@gmail.com>
2020-11-15 15:24:56 +0000

    ajout village

--- src/main/java/sn/simplon/controller/ClientController.java
+++ src/main/java/sn/simplon/controller/ClientController.java
@@ -1,6 +1,6 @@
 package sn.simplon.controller;
 
-import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,21 +8,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import sn.simplon.dao.IClientDao;
-import sn.simplon.entities.Client;
+import sn.simplon.dao.IVillageDao;
+
 
 @Controller
+@RequestMapping(value="/client")
 public class ClientController {
 	
-
-	
-    @Autowired
-    private IClientDao IClientDao;
-    
-//    @RequestMapping(value="Client/add")	
-//////	public String listeClient() {
-////    public ModelAndView listeClient() {
-//    	List<Client> client = IClientDao.findAll();
-//		
+	 
+	@Autowired
+	private IClientDao cliendao;
+		
    
 		
 	}
--- src/main/java/sn/simplon/controller/VillageController.java
+++ src/main/java/sn/simplon/controller/VillageController.java
@@ -1,5 +1,44 @@
 package sn.simplon.controller;
 
-public class VillageController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import sn.simplon.dao.IVillageDao;
+import sn.simplon.entities.Village;
+
+
+
+
+@Controller
+@RequestMapping(value="/village")
+public class VillageController {
+	
+	@Autowired
+	private IVillageDao villagedao;
+	
+	
+	
+	
+	
+	@PostMapping(value="/add")
+	public String add(String nomVillage) {
+		Village v = new Village();
+		v.setNomVillage(nomVillage);
+	    villagedao.save(v);
+	   
+		return "redirect:/village/"; 
+		
+	}
+
+	@GetMapping(value="/")
+	public String index(Model model) {
+		
+		model.addAttribute("listeVillage", villagedao.findAll());
+		return "village/addVillage";
+		
+	}
 }
